b16fcbdb99e9f88ca7b1869028dcab8ffa218d79
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-26 20:56:20 +0400

    mock data created for staff

---
+++ Mock/staff.ts
@@ -0,0 +1,62 @@
+export const staffData = [
+  {
+    id: 1,
+    FirstName: "Kamran",
+    LastName: "Guliev",
+    Email: "kamranguliev@gmail.com",
+    PhoneNumber: "0555555555",
+    DateOfBirth: "12.05.2024",
+    Gender: "male",
+    ProfilePhotoUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSTeb92r3ty8S9cUANvLIdaga2EVWRctF-KonHk5AlmGUdRZ3fluqHTRqXeb0oJCYkX9LSOqmdiWNF-KRK2UihWuw",
+    JobType: "Personal Trainer",
+  },
+  {
+    id: 1,
+    FirstName: "Kamran",
+    LastName: "Guliev",
+    Email: "kamranguliev@gmail.com",
+    PhoneNumber: "0555555555",
+    DateOfBirth: "12.05.2024",
+    Gender: "male",
+    ProfilePhotoUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSTeb92r3ty8S9cUANvLIdaga2EVWRctF-KonHk5AlmGUdRZ3fluqHTRqXeb0oJCYkX9LSOqmdiWNF-KRK2UihWuw",
+    JobType: "Personal Trainer",
+  },
+  {
+    id: 1,
+    FirstName: "Kamran",
+    LastName: "Guliev",
+    Email: "kamranguliev@gmail.com",
+    PhoneNumber: "0555555555",
+    DateOfBirth: "12.05.2024",
+    Gender: "male",
+    ProfilePhotoUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSTeb92r3ty8S9cUANvLIdaga2EVWRctF-KonHk5AlmGUdRZ3fluqHTRqXeb0oJCYkX9LSOqmdiWNF-KRK2UihWuw",
+    JobType: "Personal Trainer",
+  },
+  {
+    id: 1,
+    FirstName: "Kamran",
+    LastName: "Guliev",
+    Email: "kamranguliev@gmail.com",
+    PhoneNumber: "0555555555",
+    DateOfBirth: "12.05.2024",
+    Gender: "male",
+    ProfilePhotoUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSTeb92r3ty8S9cUANvLIdaga2EVWRctF-KonHk5AlmGUdRZ3fluqHTRqXeb0oJCYkX9LSOqmdiWNF-KRK2UihWuw",
+    JobType: "Personal Trainer",
+  },
+  {
+    id: 1,
+    FirstName: "Kamran",
+    LastName: "Guliev",
+    Email: "kamranguliev@gmail.com",
+    PhoneNumber: "0555555555",
+    DateOfBirth: "12.05.2024",
+    Gender: "male",
+    ProfilePhotoUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSTeb92r3ty8S9cUANvLIdaga2EVWRctF-KonHk5AlmGUdRZ3fluqHTRqXeb0oJCYkX9LSOqmdiWNF-KRK2UihWuw",
+    JobType: "Personal Trainer",
+  },
+];
