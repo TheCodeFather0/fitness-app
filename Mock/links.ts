da4449d16f6dce6ff859f7ac133ca87cd6511fcd
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-26 19:50:27 +0400

    created links mock data

---
+++ Mock/links.ts
@@ -0,0 +1,7 @@
+import { ILinks } from "@/Interface/links";
+
+export const links: ILinks[] = [
+  { id: 0, title: "Heyyət", to: "/" },
+  { id: 1, title: "İstifadəçilər", to: "/" },
+  { id: 2, title: "Kartlar", to: "/" },
+];
