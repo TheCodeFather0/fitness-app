85feabcaa60346bb517aee819c99fdd693bd919a
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-26 20:56:11 +0400

    mock data created for sidebar links

--- Mock/links.ts
+++ Mock/links.ts
@@ -1,7 +1,7 @@
 import { ILinks } from "@/Interface/links";
 
 export const links: ILinks[] = [
-  { id: 0, title: "Heyyət", to: "/" },
-  { id: 1, title: "İstifadəçilər", to: "/" },
-  { id: 2, title: "Kartlar", to: "/" },
+  { id: 0, title: "Heyyət", to: "/staff" },
+  { id: 1, title: "İstifadəçilər", to: "/users" },
+  { id: 2, title: "Kartlar", to: "/cards" },
 ];
