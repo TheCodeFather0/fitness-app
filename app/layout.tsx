e518e4d580ae48c77d0ceb1aa202a6038048f13e
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-28 20:40:55 +0400

    added poppins and changed structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Navbar";
 import Sidebar from "@/Components/Sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const poppins = Poppins({
+  weight: "100",
 });
 
 export const metadata: Metadata = {
@@ -26,13 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <div className="grid grid-cols-[300px_auto] border-t-5 border-t-black">
+      <body className={poppins.className}>
+        <div className="flex">
           <Sidebar />
-          <div className="bg-slate-200 h-[calc(100vh-85px)] overflow-auto">
+          <div className="w-full bg-[#F6F9F2]">
+            <Navbar />
             {children}
           </div>
         </div>
