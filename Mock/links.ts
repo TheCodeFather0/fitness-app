27c2a59524f2e2b5977c8e829dece3678cf46f99
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-28 19:53:33 +0400

    added icons

--- Mock/links.ts
+++ Mock/links.ts
@@ -1,7 +1,12 @@
+import { CiUser } from "react-icons/ci";
+import { GoPeople } from "react-icons/go";
 import { ILinks } from "@/Interface/links";
+import { FaRegAddressCard } from "react-icons/fa";
+import { MdOutlineDashboard } from "react-icons/md";
 
 export const links: ILinks[] = [
-  { id: 0, title: "Heyyət", to: "/staff" },
-  { id: 1, title: "İstifadəçilər", to: "/users" },
-  { id: 2, title: "Kartlar", to: "/cards" },
+  { id: 0, title: "Panel", to: "/", icon: MdOutlineDashboard },
+  { id: 1, title: "Heyyət", to: "/staff", icon: GoPeople },
+  { id: 2, title: "İstifadəçilər", to: "/users", icon: CiUser },
+  { id: 3, title: "Kartlar", to: "/cards", icon: FaRegAddressCard },
 ];
