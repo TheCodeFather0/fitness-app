64c31bc6553ce1ef9991f5a9d89488369562610b
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-28 20:42:13 +0400

    navbar completed

--- Components/Navbar/index.tsx
+++ Components/Navbar/index.tsx
@@ -4,20 +4,18 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="px-5 bg-slate-600">
-      <div className="flex justify-between items-center h-[80px] text-white">
-        <Link href="/" className="font-bold text-2xl">
-          KamRun FitNess
-        </Link>
-        <div className="flex items-center gap-2">
-          <Image
-            src="/avatar.jpg"
-            alt="user avatar"
-            width={50}
-            height={50}
-            className="rounded-full border-3 border-white"
-          />
-        </div>
+    <div className="flex justify-between px-5 items-center h-[80px] bg-white">
+      <Link href="/" className="font-bold text-2xl">
+        <Image src="/logo.png" alt="logo" width={150} height={150} />
+      </Link>
+      <div className="flex items-center gap-2">
+        <Image
+          src="/avatar.jpg"
+          alt="user avatar"
+          width={50}
+          height={50}
+          className="rounded-full border-3 border-[#7BBE47]"
+        />
       </div>
     </div>
   );
