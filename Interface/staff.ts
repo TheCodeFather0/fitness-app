031b3e35e0ea8364bb3f1369a745c3066fa854bc
TheCodeFather0 <raminmammadov056@gmail.com>
2025-04-02 20:32:37 +0400

    interface created for staff user

---
+++ Interface/staff.ts
@@ -0,0 +1,11 @@
+export interface IStaff {
+  id: string | number;
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  PhoneNumber: string;
+  DateOfBirth: string;
+  Gender: string;
+  ProfilePhotoUrl: string;
+  JobType: string;
+}
